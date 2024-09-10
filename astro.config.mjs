d713ffc10f1770b4528ef59e99823b73e5e398ac
kanakanho <kahoshibakichi@gmail.com>
2024-09-11 02:31:05 +0900

    fix: github pages用の設定の変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: process.env.GITHUB_PAGES ? "astro-test" : "./",
+  site: 'https://kanakanho.github.io',
+  base: 'astro-test',
 });
